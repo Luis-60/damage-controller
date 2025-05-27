87db279af02a25aee111d7f1a68deefa6b706db3
kaullango <146358364+kaullango@users.noreply.github.com>
2025-05-27 18:48:21 -0300

    testes

---
+++ backend/contador.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+arquivo_csv = r'C:\Users\kalve\Documents\damage-controller\backend\Banco_de_Dados 2.CSV'
+
+
+
+df = pd.read_csv(arquivo_csv, encoding='latin1', sep=';', on_bad_lines='skip')
+
+
+contador_def=df['DEFEITOS'].value_counts()
+
+print(contador_def)
